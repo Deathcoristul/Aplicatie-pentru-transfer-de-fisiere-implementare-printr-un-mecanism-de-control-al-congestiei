8b0c377e892a1a8b4c5bf7bd4cba8447adba4f5c
Ștefan-Adrian Iliescu <stefanadrian2008@gmail.com>
2021-12-01 17:28:47 +0200

    Just the interface

---
+++ main.py
@@ -0,0 +1,5 @@
+from tkinter import *
+from window import *
+if __name__ == '__main__':
+    app=App()
+    app.mainloop()
---
+++ window.py
@@ -0,0 +1,23 @@
+import tkinter
+from tkinter import *
+from tkinter import ttk
+from tkinter.messagebox import showinfo
+from tkinter.filedialog import *
+class App(Tk):
+    def button_clicked(self):
+        Tk().withdraw()
+        filename=askopenfilename()
+        self.text.delete('1.0',END)
+        self.text.insert(END,filename)#print(filename)
+    def __init__(self):
+        super().__init__()
+        super().title("Aplicatie pentru transfer de fisiere implementare printr-un mecanism de control al congestiei")
+        self.geometry=('1900x700')
+        self.label=ttk.Label(self,text='Salut!')
+        self.label.pack()
+        self.text=Text(self,height=1)
+        #self.text.insert(tkinter.END,'\n')
+        self.text.pack(side=tkinter.LEFT)
+        self.button=ttk.Button(self,text='Choose',command=self.button_clicked)
+        self.button.pack()
+
